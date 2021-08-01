96a2d1a8872a76594c32598a1c5f099ef7d6a4d3
Csaba Soti <sotcsa@gmail.com>
2021-08-01 19:18:00 +0200

    Fist_deployable

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,4 +1,7 @@
-// inside instruction.rs
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+use crate::error::EscrowError::InvalidInstruction;
+
 pub enum EscrowInstruction {
 
     /// Starts the trade by creating and populating an escrow account and transferring ownership of the given temp token account to the PDA
@@ -15,5 +18,28 @@ pub enum EscrowInstruction {
     InitEscrow {
         /// The amount party A expects to receive of token Y
         amount: u64
+    }
+}
+
+impl EscrowInstruction {
+    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
     }
 }
